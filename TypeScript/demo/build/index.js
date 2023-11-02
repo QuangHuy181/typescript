18601a661d7f498079e5688a3fc2b1b1622917d1
Pha <deviljoker181@gmail.com>
2023-11-02 11:05:43 +0700

    demo

---
+++ TypeScript/demo/build/index.js
@@ -0,0 +1,7 @@
+"use strict";
+let a = 10;
+let b = 11;
+let c = 12;
+console.log(a);
+console.log(b);
+console.log(c);
---
+++ TypeScript/demo/src/index.ts
@@ -0,0 +1,6 @@
+let a:number = 10;
+let b:number = 11;
+let c:number = 12;
+console.log(a);
+console.log(b);
+console.log(c);
